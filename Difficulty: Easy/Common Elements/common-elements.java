c6c74b2e342893366cd8bfc126886525317c452d
Navya Shukla <navyaa.shukla2812@gmail.com>
2026-02-12 09:52:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Common Elements/common-elements.java
@@ -0,0 +1,27 @@
+// User function Template for Java
+
+class Solution {
+    public static ArrayList<Integer> commonElements(int a[], int b[]) {
+        // Your code here
+       Arrays.sort(a);
+       Arrays.sort(b);
+       int i=0;
+       int j=0;
+       ArrayList<Integer> ans=new ArrayList<>();
+       while (i < a.length && j < b.length){
+           if(a[i]==b[j]){
+               ans.add(a[i]);
+               i++;
+               j++;
+           }
+           else if(a[i]<b[j]){
+               i++;
+           }
+           else{
+               j++;
+           }
+       }
+       return ans;
+       
+    }
+}
