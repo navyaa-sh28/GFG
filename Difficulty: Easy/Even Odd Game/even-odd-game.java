08b813076115bd155614e07113c56b12607c2d78
Navya Shukla <navyaa.shukla2812@gmail.com>
2026-02-13 00:05:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Even Odd Game/even-odd-game.java
@@ -0,0 +1,16 @@
+// User function Template for Java
+class Solution {
+    public static String utility(int n) {
+
+        // write your code here
+        if(n%2==0&&n!=1){
+            return "Friend";
+        }
+        else if(n==1){
+            return"You";
+        }
+        else{
+            return"You";
+        }
+    }
+}
