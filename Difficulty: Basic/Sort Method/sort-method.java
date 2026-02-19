7dce99c453e7cbde070fdd71fee548ee8725ddd2
Navya Shukla <navyaa.shukla2812@gmail.com>
2026-02-19 10:09:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sort Method/sort-method.java
@@ -0,0 +1,8 @@
+// User function Template for Java
+class Solution {
+    public static void sortArray(int arr[]) {
+        // Your code here
+        Arrays.sort(arr);
+        // You do not need to print
+    }
+}
