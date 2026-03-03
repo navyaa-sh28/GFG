23c34123c75df8fb53b106fec16ff93410c8fc4d
Navya Shukla <navyaa.shukla2812@gmail.com>
2026-03-03 23:15:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Table Difference/table-difference.java
@@ -0,0 +1,12 @@
+// User function Template for Java
+class Solution {
+    public static void difference(int n1, int n2) {
+
+        // Write your code here
+        for(int i=1;i<11;i++){
+            int a=n1*i;
+            int b=n2*i;
+            System.out.print(a-b+" ");
+        }
+    }
+}
