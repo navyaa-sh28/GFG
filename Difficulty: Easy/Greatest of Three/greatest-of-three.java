b7998077c495947a9e4e638f5e1f8ea46c356faa
Navya Shukla <navyaa.shukla2812@gmail.com>
2026-03-19 23:33:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Greatest of Three/greatest-of-three.java
+++ Difficulty: Easy/Greatest of Three/greatest-of-three.java
@@ -1,21 +1,29 @@
-// User function Template for Java
-class Solution {
+//Back-end complete function Template for Java
+import java.util.*;
 
-    public static int find_greatest_number(int a, int b, int c) {
-        // Write your code here..
+public class Solution {
+    public static void main(String args[]) {
+        // Your code here
+        int a,b,c;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=sc.nextInt();
         if(a>b){
             if(a>c){
-                return a;
+                System.out.println(a);
             }
             else{
-                return c;
+                System.out.println(c);
             }
         }
-        else if(b>c){
-            return b;
-        }
         else{
-            return c;
+            if(b>c){
+                System.out.println(b);
+            }
+            else{
+                System.out.println(c);
+            }
         }
     }
 }
