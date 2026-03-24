f86871ca7244d665b2a2198c536bb2222a278bb5
Navya Shukla <navyaa.shukla2812@gmail.com>
2026-03-25 00:55:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Java Inheritance/java-inheritance.java
@@ -0,0 +1,13 @@
+class cls2 extends cls1 {
+
+    // mul method is already given in problem statement
+    void mul(int p, int q) {
+        System.out.println(p * q);
+    }
+
+    // task method to print sum of squares
+    void task(int p, int q) {
+        int sumOfSquares = p * p + q * q;
+        System.out.println(sumOfSquares);
+    }
+}
