53405f2a323404c5aecac245aa84aa069034081b
Navya Shukla <navyaa.shukla2812@gmail.com>
2026-04-15 22:43:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/LinkedList Traversal/linked-list-traversal.java
@@ -0,0 +1,21 @@
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node(int val) {
+        data = val;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    void printList(Node head) {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+    }
+}
