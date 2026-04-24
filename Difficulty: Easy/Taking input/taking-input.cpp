8ecb61905c1a8e55a773fc27daecc79756e5b48c
Navya Shukla <navyaa.shukla2812@gmail.com>
2026-04-24 21:34:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Taking input/taking-input.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    void inputData() {
+        // code here
+        int a;
+        string s;
+        cin>> a;
+        cin>> s;
+        cout<<a<<" "<<s<<endl;
+    }
+};
