ae6b1ebbd8105ebba64006ffbdf5673a8e6e4833
Navya Shukla <navyaa.shukla2812@gmail.com>
2026-05-02 22:00:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/The Else Statement/the-else-statement.java
+++ Difficulty: Basic/The Else Statement/the-else-statement.java
@@ -1,15 +1,16 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class Solution {
+class GFG {
     public static void main(String args[]) {
-        // Your Code Here
-        Scanner sc=new Scanner(System.in);
-        int a=sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
         if(a>100){
             System.out.println("Big");
         }
         else{
             System.out.println("Small");
         }
+        
     }
 }
