f0c2da97391b266f79c42d7610f2090cd9ad48bc
Navya Shukla <navyaa.shukla2812@gmail.com>
2026-05-09 11:42:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/C++ Operators (Relational) | Set 2/c-operators-relational-set-2.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    string compareNum(int A, int B) {
+        if (A > B) {
+            return to_string(A) + " is greater than " + to_string(B);
+        }
+        else if (A < B) {
+            return to_string(A) + " is less than " + to_string(B);
+        }
+        else {
+            return to_string(A) + " is equals to " + to_string(B);
+        }
+    }
+};
