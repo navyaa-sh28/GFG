3afbd344ea2508f4b98236bb26154aad502895ad
Navya Shukla <navyaa.shukla2812@gmail.com>
2026-05-14 13:21:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/C++ 2-D Arrays | Set-1/c-2d-arrays-set1.cpp
@@ -0,0 +1,11 @@
+vector<vector<int>> transpose(int a[][M], int n) {
+    vector<vector<int>> ans(n, vector<int>(n));
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            ans[i][j] = a[j][i];
+        }
+    }
+
+    return ans;
+}
