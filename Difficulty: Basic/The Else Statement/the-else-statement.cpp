8ffea3b5958115d7898eff0354e98c2a8c44179a
Navya Shukla <navyaa.shukla2812@gmail.com>
2026-06-01 21:34:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/The Else Statement/the-else-statement.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+
+    // code here
+    if(a>100){
+        cout<<"Big"<<endl;
+    }
+    else{
+        cout<<"Small"<<endl;
+    }
+
+    return 0;
+}
